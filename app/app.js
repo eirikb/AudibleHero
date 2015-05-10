88af581398d23260c3214220b7a745e34e4858bc
Eirik Brandtzæg <eirikb@eirikb.no>
2015-05-10 10:28:42 +0200

    Conditionally only add AudibleHero-button to homepage

--- app/app.js
+++ app/app.js
@@ -1,3 +1,6 @@
-var script = document.createElement('script');
-script.src = '//eirikb.blob.core.windows.net/audiblehero/dev-eirik/app.js';
-document.head.appendChild(script);
+var path = location.pathname.replace(/(ref=.*|\/)/g, '');
+if (!path) {
+  var script = document.createElement('script');
+  script.src = '//eirikb.blob.core.windows.net/audiblehero/dev-eirik/app.js';
+  document.head.appendChild(script);
+}
