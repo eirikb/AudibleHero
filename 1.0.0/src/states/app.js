be9389ec955b0931f537c5d7cfaa6eeea502f7c5
Eirik Brandtzæg <eirikb@eirikb.no>
2015-05-21 20:53:48 +0200

    Calc hours from duration on given books

--- 1.0.0/src/states/app.js
+++ 1.0.0/src/states/app.js
@@ -1,17 +1,13 @@
 var app = require('../app.js');
 require('lodash');
 
-function byOwned(group) {
-  return {
-    all: group,
-    owned: _.where(group, {owned: true}),
-    missing: _.reject(group, 'owned')
+app.filter('filterBooks', function () {
+  return function (input, scope) {
+    return _.filter(input, function (book) {
+      return book.owned && scope.showOwned || !book.owned && scope.showMissing;
+    })
   };
-}
-
-function byOwnedAndMissing(group) {
-  return group.owned.length > 0 && group.missing.length > 0;
-}
+});
 
 app.config(function ($stateProvider) {
   $stateProvider.state('app', {
@@ -20,27 +16,7 @@ app.config(function ($stateProvider) {
 
       var data = $scope.data = loadFromStorage();
       if (data) {
-        $scope.owned = _.where(data.books, {owned: true});
-
-        $scope.series = _(books).filter('seriesId').groupBy('seriesId').map(byOwned).filter(byOwnedAndMissing).map(function (group) {
-          var b = group.owned[0];
-          group.key = b.authors.join(',') + ' - ' + b.series.name;
-          return group;
-        }).value();
-
-        $scope.authors = _(books).map(function (book) {
-          return _.map(book.authors, function (author) {
-            return _.assign({
-              author: author
-            }, book);
-          });
-        }).flatten().groupBy('author').map(byOwned).filter(byOwnedAndMissing).map(function (group) {
-          var b = group.owned[0];
-          group.key = b.author;
-          if (b.series) group.key += " - " + b.series.name;
-          return group;
-        }).value();
-
+        $scope.books = data.books;
       } else {
         $state.go('load');
       }
@@ -49,7 +25,12 @@ app.config(function ($stateProvider) {
         return _.where($scope.data.books, prop);
       };
 
-      $scope.calcListenTime = function () {
+      $scope.calcListenTime = function (books) {
+        if (books) {
+          return Math.floor(_.sum(books), function (book) {
+            return book.duration ? book.duration.asHours() : 0;
+          });
+        }
         return Math.floor(_.sum($scope.filter({downloaded: true}), function (book) {
           return book.duration ? book.duration.asHours() : 0;
         }));
