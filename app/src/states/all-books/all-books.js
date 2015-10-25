9d1780018963ad9c74c28467e86890a9dbc170a9
Eirik Brandtzæg <eirikb@eirikb.no>
2015-10-25 13:06:38 +0100

    Add ignore/un-ignore-button to all books table

--- app/src/states/all-books/all-books.js
+++ app/src/states/all-books/all-books.js
@@ -2,8 +2,13 @@ angular.module('audiblehero').config(function ($stateProvider) {
   $stateProvider.state('app.allbooks', {
     url: "allbooks",
     template: require('./all-books.html'),
-    controller: function ($scope) {
+    controller: function ($scope, saveIgnored) {
       $scope.displayBooks = [].concat($scope.books);
+
+      $scope.toggleIgnore = function (book) {
+        book.ignored = !book.ignored;
+        saveIgnored();
+      };
     }
   });
 });
