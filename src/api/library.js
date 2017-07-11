827e272ebedf7dd837d530c46e3068f18dc6058f
Eirik Brandtzæg <eirikb@eirikb.no>
2017-07-11 22:47:22 +0200

    Library api
    Started on fundamentals for library api.

---
+++ src/api/library.js
@@ -0,0 +1,25 @@
+export default () => {
+
+  console.log('loading library...');
+  return fetch('/lib-ajax?progType=all&timeFilter=all&itemsPerPage=100000', {
+    credentials: 'include'
+  }).then(r =>
+    r.text()
+  ).then(html => {
+    console.log('ok');
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+
+    const rows = Array.from(doc.querySelectorAll('[name=productCover]')).map(e => e.parentNode);
+
+    return rows.map(row => {
+      const title = row.querySelector('h3').innerText;
+      const authors = row.querySelector('.adbl-library-item-author').innerText.split(',');
+      const rating = parseInt(row.querySelector('.adbl-rating-num').innerText.match(/\d+/)[0]);
+
+      return {
+        title, authors, rating
+      };
+    });
+  });
+}
