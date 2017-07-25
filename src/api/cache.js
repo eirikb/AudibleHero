99227dcc8832ebd76f4319b89614961079477717
Eirik Brandtzæg <eirikb@eirikb.no>
2017-07-25 23:52:22 +0200

    Cache for books
    Hard coded to work for books specifically, turning the size in
    localStorage to the smallest possible.
    Using array position instead of keys, storing keys in a separate
    array.
    Using lz-string to compress data in localStorage.
    Loads data initially on app open, stores on every call to "set"
    method.

---
+++ src/api/cache.js
@@ -0,0 +1,40 @@
+import {compress, decompress} from 'lz-string';
+
+const version = require('../../package.json').version;
+
+let data = null;
+
+const save = () => {
+  if (!data) {
+    data = {version, values: {}};
+  }
+  localStorage.audibleherocache = compress(JSON.stringify(data));
+};
+
+try {
+  data = JSON.parse(decompress(localStorage.audibleherocache)) || {};
+  if (data && data.version !== version) {
+    data = null;
+  }
+} catch (ignored) {
+}
+
+save();
+
+export const get = key => {
+  const value = data.values[key];
+  if (!value) return null;
+
+  const keys = value[0].sort();
+  return value[1].map(val => val.reduce((res, val, index) => {
+    const key = keys[index];
+    res[key] = val;
+    return res;
+  }, {}));
+}
+
+export const set = (key, value) => {
+  const keys = Object.keys(value[0]).sort();
+  data.values[key] = [keys, value.map(val => keys.map(key => val[key]))];
+  save();
+};
