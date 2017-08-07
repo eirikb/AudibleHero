937999d032094d9f37f183119299a327a0abdbc2
Eirik Brandtzæg <eirikb@eirikb.no>
2017-08-07 23:34:25 +0200

    Book filter
    Example filter with inLibrary, sorting and descending.

--- src/api/filter-books.js
+++ src/api/filter-books.js
@@ -1,6 +1,11 @@
 export default (books, filter = {}) => {
 
   const limit = filter.limit || 10;
+  const orderBy = filter.orderBy || 'title';
 
-  return books.slice(0, limit);
+  books = books
+    .filter(book => filter.inLibrary && filter.inLibrary === book.inLibrary || true)
+    .sort((a, b) => (a[orderBy] || '').localeCompare(b[orderBy] || ''));
+
+  return filter.desc ? books.reverse() : books;
 };
