b96377f31629849911bed08750ae4bfbf4b1038c
Eirik Brandtzæg <eirikb@eirikb.no>
2017-08-07 23:19:01 +0200

    Order authors in Update component by name
    Add button for clearing cache

--- src/components/Update.vue
+++ src/components/Update.vue
@@ -3,6 +3,12 @@
     <button @click="update">Update</button>
     <router-link to="books" :disabled="$store.state.books.length === 0">Skip</router-link>
 
+    <div v-if="$store.state.books.length > 0">
+      {{$store.state.books.length}} books in cache<br>
+      <button @click="clearCache">Clear cache</button>
+    </div>
+
+
     <p>
       library progress: {{$store.state.progressLibrary}}
       <Progress :progress="$store.state.progressLibrary"></Progress>
@@ -10,9 +16,9 @@
 
     <div class="mdc-layout-grid">
       <div class="mdc-layout-grid__inner">
-        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-2" v-for="(progress, author) in $store.state.progressAuthors">
-          <b>{{author}}</b>
-          <Progress :progress="progress"></Progress>
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-2" v-for="author in authors">
+          <b>{{author.name}}</b>
+          <Progress :progress="author.progress"></Progress>
         </div>
       </div>
     </div>
@@ -20,11 +26,28 @@
 </template>
 
 <script>
+  import {clear, save} from '../api/cache';
+
   export default {
+    computed: {
+      authors() {
+        return Object.entries(this.$store.state.progressAuthors)
+          .map(([name, progress]) => ({name, progress}))
+          .sort((a, b) => a.name.localeCompare(b.name));
+      }
+    },
+
     methods: {
       async update() {
         await this.$store.dispatch('update');
+        this.$store.dispatch('getBooks');
         this.$router.push('/books');
+      },
+
+      clearCache() {
+        clear();
+        save();
+        this.$tore.dispatch('getBooks');
       }
     }
   }
