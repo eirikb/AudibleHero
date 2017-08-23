a5ce852db75bdd8ab95cd437e61afdcafbc8995e
Eirik Brandtzæg <eirikb@eirikb.no>
2017-08-23 23:32:27 +0200

    Speed up lazy directive
    By not re-setting src to blank for every element the speed
    will for some reason increase.

--- src/lazy.js
+++ src/lazy.js
@@ -2,13 +2,17 @@ import Vue from 'vue';
 
 Vue.directive('lazy', {
   bind(el) {
+    el.src = '';
+
     el.__onScroll__ = () => {
       const rect = el.getBoundingClientRect();
       const height = el.height;
 
       const elementIsVisible = rect.top + height >= 0 && rect.bottom - height <= window.innerHeight;
 
-      el.src = elementIsVisible ? el.dataset.src : '';
+      if (elementIsVisible) {
+        el.src =  el.dataset.src;
+      }
     };
     document.addEventListener('scroll', el.__onScroll__);
   },
