1ecee24316745e7080537e183a57aa5a2b9d9b88
Eirik Brandtzæg <eirikb@eirikb.no>
2017-09-08 22:53:28 +0200

    Add changelog
    Shown in the update-view

--- src/components/Update.vue
+++ src/components/Update.vue
@@ -40,6 +40,39 @@
         </div>
       </div>
     </div>
+    <div v-else class="mdc-layout-grid">
+      <div class="mdc-layout-grid__inner">
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+          <h3>2017-09-08</h3>
+          <p>A new and worse AudibleHero!</p>
+          <p>
+            I've had several issues lately with the extension, so I had to do a lot of fixes.<br>
+            In the end I decided to re-write the extension, in order to make it easier
+            to maintain and add new features.<br>
+            This means some features are gone (until someone request them back), such
+            as Incognito search.<br>
+            Remember I'm making this extension mainly for myself, as I'm doing it in my spare time, for free.
+          </p>
+          <p>
+            Noteworthy changes:
+          </p>
+          <ul>
+            <li>New look.<br>Replaced Bootstrap with Material Design, so now everything looks worse, but that's probably because I don't know how to use it properly.
+            </li>
+            <li>Removed Incognito search.<br>I simply no longer bother to do the hack to get other books.</li>
+            <li>Removed Ignore books.<br>Storing data on the Google account didn't work properly. All data is now stored on the computer.
+            </li>
+          </ul>
+          <p>
+            Please file all bugs and/or feature requests to the <a target="_blank"
+                                                                   href="https://github.com/eirikb/audiblehero">GitHub Repository</a>.<br>
+            If you don't have a GitHub account, and don't want one, you can e-mail me at <a
+              href="mailto:eirikb@eirikb.no">eirikb@eirikb.no</a>.
+          </p>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
