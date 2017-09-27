6de8d49b982671af2ddccfb370af07a898f15491
Eirik Brandtzæg <eirikb@eirikb.no>
2017-09-27 22:14:28 +0200

    Hide original page (legal-term error page) on start
    When loading the extension the default error-page
    for the legal-terms page would show.
    This fix will hide that content until app is loaded.

--- ext/init.js
+++ ext/init.js
@@ -1,3 +1,5 @@
 document.title = 'AudibleHero!';
 document.querySelector('style').innerText = '';
-document.body.innerHTML = '<div id="app"><router-view>Loading...</router-view></div>';
+document.body.innerHTML = '<div id="app"><router-view></router-view></div>';
+document.body.className = 'show';
+document.querySelector('html').className = 'show';
