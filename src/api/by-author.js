4af47502d116a0b7140c23a8b85a19e20f5c1283
Eirik Brandtzæg <eirikb@eirikb.no>
2018-02-02 23:31:37 +0100

    Update by-author API
    Turns out Audible have changed the markup of the author search page.
    I hope the new queries work, tested on my own library, and it seems
    to do the trick.

--- src/api/by-author.js
+++ src/api/by-author.js
@@ -12,7 +12,7 @@ const toDate = match => {
     const month = padZero(match[1]);
     const day = padZero(match[0]);
     return [year, month, day].join('-');
-  } else  {
+  } else {
     // mm-dd-yy
     const year = (+match[2] > 50 ? '19' : '20') + match[2];
     const month = padZero(match[0]);
@@ -21,34 +21,36 @@ const toDate = match => {
   }
 };
 
-export default (author, limit, page) => fetch(`/search?searchRank=-publication_datesearch&searchSize=${limit}&searchPage=${page}&searchAuthor=${author}`, {
+export default (author, limit, page) => fetch(`/search?keywords=${author}&sort=pubdate-desc-rank&pageSize=${limit}&page=${page}`, {
   credentials: 'include'
 }).then(r =>
   r.text()
 ).then(async html => {
   const doc = parse(html);
 
-  const rows = Array.from(doc.querySelectorAll('.adbl-result-item'));
+  const rows = Array.from(doc.querySelectorAll('.productListItem'));
 
   const books = await Promise.all(rows.map(async row => {
-    const id = row.querySelector('[name=productAsin]').value;
-    const title = row.querySelector('.adbl-prod-title').innerText.trim();
+    const heading = row.querySelector('.bc-heading a');
+    const id = heading.href.match(/(\w+)\?/)[1];
+
+    const title = heading.innerText.trim();
 
     const byRegex = regex => Array.from(row.querySelectorAll('li'))
       .map(node => ({
         text: node.innerText.trim(), node
       })).find(res => res.text.match(regex)) || {};
 
-    const authors = Array.from(row.querySelectorAll('.adbl-prod-author a')).map(e => e.innerText);
+    const authors = Array.from(row.querySelectorAll('.authorLabel a')).map(e => e.innerText);
     const lengthText = byRegex(/^Length:/).text;
     let match = lengthText.match(/(\d+) hr/);
     const hours = match ? parseInt(match[1]) : 0;
     match = lengthText.match(/(\d+) min/);
     const mins = match ? parseInt(match[1]) : 0;
     const length = mins + hours * 60;
-    const imageId = last(((row.querySelector('.adbl-prod-image') || {}).src || '').split('/')).split('.')[0];
+    const imageId = last(((row.querySelector('.bc-lazy-load') || {}).src || '').split('/')).split('.')[0];
 
-    const releaseDateText = byRegex(/Release Date:/).text;
+    const releaseDateText = byRegex(/Release date:/).text;
     match = (releaseDateText || '').match(/(\d+)[-\/](\d+)[-\/](\d+)/);
     const releaseDate = match ? toDate(match.slice(1)) : null;
 
@@ -57,14 +59,14 @@ export default (author, limit, page) => fetch(`/search?searchRank=-publication_d
     const seriesNode = byRegex(/^Series:/).node;
     let seriesName = '';
     if (seriesNode) {
-      seriesId = last(seriesNode.querySelector('a').href.split('='));
+      seriesId = seriesNode.querySelector('a').href.match(/asin=(\w*)/)[1];
       seriesBookIndex = seriesNode.innerText.trim().split(', Book ')[1];
       seriesName = seriesNode.querySelector('a').innerText;
     }
 
-    const rating = parseInt((((row.querySelector('.adbl-rating-num') || {}).innerText || '').match(/\d+/) || [])[0]) || 0;
+    const rating = parseInt((((row.querySelector('.ratingsLabel') || {}).innerText || '').match(/\d+/g) || []).join('')) || 0;
 
-    const description = (row.querySelector('.socialTile-summary p') || {}).innerText || '';
+    const description = (row.querySelector(`#product-list-flyout-${id} p`) || {}).innerText || '';
 
     const languageRes = await detectLanguage(description);
 
