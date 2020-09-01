c763a8dce690e3a8edee3b413083c321d54f87d0
Eirik Brandtzæg <eirikb@eirikb.no>
2020-09-01 13:39:33 +0200

    Setup main

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,7 @@
-import 'babel-polyfill';
-import Vue from 'vue';
-import store from './store';
-import router from './router';
-import './lazy';
-import './analytics'
+import { init } from './domdom';
+import App from './views/App';
+import data from './data';
+import './main.scss';
 
-const req = require.context('./components/', true, /\.(js|vue)$/i);
-req.keys().map(key => {
-  const name = key.match(/\w+/)[0];
-  return Vue.component(name, req(key))
-});
-
-new Vue({
-  el: '#app',
-  store,
-  router
-});
+init(document.body, App);
+data();
