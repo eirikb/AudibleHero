44b4c95f3891ebc34b479c15fb924ef77b87f8be
Eirik Brandtzæg <eirikb@eirikb.no>
2020-09-01 13:37:07 +0200

    Components
    Strict!

--- src/components.tsx
+++ src/components.tsx
@@ -1,7 +1,11 @@
 import { React } from './domdom';
 import { MDCRipple } from '@material/ripple';
+import { OptChildren } from '@eirikb/domdom';
 
-export const Button = ({ onClick }, { children }) => {
+export const Button: (
+  _: { onClick: () => void },
+  __: { children: OptChildren }
+) => void = ({ onClick }, { children }) => {
   const button = (
     <button className="mdc-button" onClick={onClick}>
       <span className="mdc-button__label">{children}</span>
@@ -12,19 +16,30 @@ export const Button = ({ onClick }, { children }) => {
   return button;
 };
 
-export const Grid = ({}, { children }) => (
+export const Grid: (_: any, opts: { children: OptChildren }) => void = (
+  {},
+  { children }
+) => (
   <div className="mdc-layout-grid">
     <div className="mdc-layout-grid__inner">{children}</div>
   </div>
 );
 
-export const Cell = ({ span }, { children }) => (
+export const Cell: (
+  _: { span: number },
+  __: { children: OptChildren }
+) => void = ({ span }, { children }) => (
   <div className={`mdc-layout-grid__cell mdc-layout-grid__cell--span-${span}`}>
     {children}
   </div>
 );
 
-export const Card = ({ media, title, subtitle, actions }) => (
+export const Card: (_: {
+  media: string;
+  title: string;
+  subtitle: string;
+  actions: string;
+}) => void = ({ media, title, subtitle, actions }) => (
   <div class="mdc-card">
     {media ? (
       <section className="mdc-card__media">
