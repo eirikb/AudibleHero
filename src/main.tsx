eb23db417d7926b720208ba53952694a48de20c6
Eirik Brandtzæg <eirikb@eirikb.no>
2020-09-05 23:21:58 +0200

    Initially load books

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { init, set } from './domdom';
+import { load } from './api/cache';
 import App from './views/App';
 import './main.scss';
 
 init(document.body, App);
 set('route', 'books');
+set('books', load());
