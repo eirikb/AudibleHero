e6f0944056f93a0cb52f974b49dd20c5e1aab1d6
Eirik Brandtzæg <eirikb@eirikb.no>
2020-09-05 22:31:20 +0200

    Add progress component

--- src/components.tsx
+++ src/components.tsx
@@ -1,14 +1,15 @@
-import { React } from './domdom';
+import { React, on } from './domdom';
 import { MDCRipple } from '@material/ripple';
 import { OptChildren } from '@eirikb/domdom';
+import { MDCLinearProgress } from '@material/linear-progress';
 
 export const Button: (
   _: { onClick: () => void },
   __: { children: OptChildren }
 ) => void = ({ onClick }, { children }) => {
   const button = (
-    <button className="mdc-button" onClick={onClick}>
-      <span className="mdc-button__label">{children}</span>
+    <button class="mdc-button" onClick={onClick}>
+      <span class="mdc-button__label">{children}</span>
     </button>
   );
 
@@ -20,8 +21,8 @@ export const Grid: (_: any, opts: { children: OptChildren }) => void = (
   {},
   { children }
 ) => (
-  <div className="mdc-layout-grid">
-    <div className="mdc-layout-grid__inner">{children}</div>
+  <div class="mdc-layout-grid">
+    <div class="mdc-layout-grid__inner">{children}</div>
   </div>
 );
 
@@ -29,7 +30,7 @@ export const Cell: (
   _: { span: number },
   __: { children: OptChildren }
 ) => void = ({ span }, { children }) => (
-  <div className={`mdc-layout-grid__cell mdc-layout-grid__cell--span-${span}`}>
+  <div class={`mdc-layout-grid__cell mdc-layout-grid__cell--span-${span}`}>
     {children}
   </div>
 );
@@ -42,9 +43,9 @@ export const Card: (_: {
 }) => void = ({ media, title, subtitle, actions }) => (
   <div class="mdc-card">
     {media ? (
-      <section className="mdc-card__media">
+      <section class="mdc-card__media">
         <img
-          className="mdc-card__media-item mdc-card__media-item--2x"
+          class="mdc-card__media-item mdc-card__media-item--2x"
           src={media}
         />
       </section>
@@ -52,9 +53,35 @@ export const Card: (_: {
       ''
     )}
     <section class="mdc-card__primary">
-      <h1 className="mdc-card__title mdc-card__title--large">{title}</h1>
-      <h2 className="mdc-card__subtitle">{subtitle}</h2>
+      <h1 class="mdc-card__title mdc-card__title--large">{title}</h1>
+      <h2 class="mdc-card__subtitle">{subtitle}</h2>
     </section>
     <section class="mdc-card__actions">{actions}</section>
   </div>
 );
+
+export const Progress: (_: { path: string; determinate: boolean }) => void = ({
+  path,
+  determinate,
+}) => {
+  const element = (
+    <div class="mdc-linear-progress">
+      <div class="mdc-linear-progress__buffer">
+        <div class="mdc-linear-progress__buffer-bar"></div>
+        <div class="mdc-linear-progress__buffer-dots"></div>
+      </div>
+      <div class="mdc-linear-progress__bar mdc-linear-progress__primary-bar">
+        <span class="mdc-linear-progress__bar-inner"></span>
+      </div>
+      <div class="mdc-linear-progress__bar mdc-linear-progress__secondary-bar">
+        <span class="mdc-linear-progress__bar-inner"></span>
+      </div>
+    </div>
+  );
+  const linearProgress = new MDCLinearProgress(element);
+  linearProgress.determinate = determinate;
+  on(`!+* ${path}`, progress => (linearProgress.progress = progress)).attach(
+    element
+  );
+  return element;
+};
